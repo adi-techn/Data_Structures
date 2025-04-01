30cf58dd3bb4c1003103a03f9e4ccb6a9bdcc2c2
Adi_Tech <adityasinghxa07@gmail.com>
2025-04-02 00:10:34 +0530

    Add files via upload

---
+++ subTree.java
@@ -0,0 +1,53 @@
+public class subTree {
+     static class Node{
+          int data;
+          Node left;
+          Node right;
+
+          Node(int data){
+               this.data=data;
+               this.left=null;
+               this.right=null;
+          }
+     }
+     public static boolean isSubTree(Node root,Node subRoot){
+          if(root==null){
+               return false;
+          }
+          if(root.data==subRoot.data){
+               if(isIdentical(root,subRoot)){
+                    return true;
+               }
+          }
+          return isSubTree(root.left, subRoot) || isSubTree(root.right, subRoot);
+     }
+     public static boolean isIdentical(Node node,Node subRoot){
+          if(node==null && subRoot==null){
+               return true;
+          }
+          else if(node==null || subRoot==null || node.data!=subRoot.data){
+               return false;
+          }
+          if(!isIdentical(node.left, subRoot.left)){
+               return false;
+          }
+          if(!isIdentical(node.right, subRoot.right)){
+               return false;
+          }
+          return true;
+     }
+     public static void main(String[] args) {
+          Node root=new Node(1);
+          root.left=new Node(2);
+          root.left.left=new Node(4);
+          root.left.right=new Node(5);
+          root.right=new Node(6);
+          root.right.right=new Node(7);
+
+          Node subroot=new Node(2);
+          subroot.left=new Node(4);
+          subroot.right=new Node(5);
+
+          System.out.println(isSubTree(root,subroot));
+     }
+}
